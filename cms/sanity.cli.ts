ef3fc5f0543328704372c58ac1cc2cba37163193
kmettom <kmettom@gmail.com>
2026-09-22 17:07:40 +0100

    remove project id

--- cms/sanity.cli.ts
+++ cms/sanity.cli.ts
@@ -7,7 +7,7 @@
 
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || "xdfywt0f"
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 export default defineCliConfig({
